dae558d2577351362c6451e9504f9243ed728e37
Jeroen van Langen <jvanlangen@live.nl>
2022-04-28 11:57:11 +0200

    Comment erbij gezet

--- Dobbelsteen.ino
+++ Dobbelsteen.ino
@@ -36,53 +36,47 @@ const uint8_t dobbelsteenOpmaak[ 7 ][ 7 ] = {
 // the setup function runs once when you press reset or power the board
 void setup() {
 
+  // zorgen dat random ook echt een random waarde geeft
   randomSeed(analogRead(0));
 
-  // initialize digital pin LED_BUILTIN as an output.
+  // alle digital pin LED_BUILTIN as an output.
   for (int i = 0; i < 7; i++)
     pinMode(oogLedPins[i], OUTPUT);
 
+  // button pin als input initializeren
   pinMode(btnPin, INPUT);
+  // pwr pin voor de button
   pinMode(pwrPin, OUTPUT);
 
+  // (+5v) hoog zetten, zodat we de button kunnen checken
   digitalWrite(pwrPin, HIGH);
 
+  // serial monitor checken
   Serial.begin(115200);
 }
 
+// de functie die de dobbelsteen leds aanstuurt
 void tekenDobbelsteen(int waarde) {
+  // met een for-loop checken welke leds aan en uit moeten staan
   for (int i = 0; i < 7; i++)
     digitalWrite(oogLedPins[i], dobbelsteenOpmaak[waarde][i]);
 }
 
-bool btnPressed = false;
-bool rolDeDobbelsteen = false;
-
 // the loop function runs over and over again forever
 void loop() {
-  if (digitalRead(btnPin) == HIGH) {
-    btnPressed = true;
-  }
-  else
-  {
-    if (btnPressed)
-    {
-      rolDeDobbelsteen = true;
-      btnPressed = false;
-    }
-  }
 
-  if (rolDeDobbelsteen)
-  {
-    rolDeDobbelsteen = false;
+  // kijken of de button ingedrukt wordt
+  if (digitalRead(btnPin) == HIGH) {
 
     Serial.println("rol");
-    int waarde;
-    
-    for(int i=1;i<20;i++)
+    // doe 20 keer random en zet de leds aan
+    for (int i = 1; i < 20; i++)
     {
-      waarde = random(1, 7);
+      // een random waarde tussen 1 t/m 6
+      int waarde = random(1, 7);
+      // zet leds aan/uit
       tekenDobbelsteen(waarde);
+      // wacht 100 ms.
       delay(100);
     }
   }
